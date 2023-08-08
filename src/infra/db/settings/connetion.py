37042d5fca7593b2a00078d8e974544e62897143
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-08-08 19:04:49 -0300

    feat: implementing UsersRepository

--- src/infra/db/settings/connetion.py
+++ src/infra/db/settings/connetion.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 class DBConnetionHandler:
@@ -14,6 +15,7 @@ class DBConnetionHandler:
             "clean_database"
         )
         self.__engine = self.create_database_engine()
+        self.session = None
 
     def create_database_engine(self):
         engine = create_engine(self.__connection_string)
@@ -21,3 +23,11 @@ class DBConnetionHandler:
 
     def get_engine(self):
         return self.__engine
+
+    def __enter__(self):
+        session_maker = sessionmaker(bind=self.__engine)
+        self.session = session_maker()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
