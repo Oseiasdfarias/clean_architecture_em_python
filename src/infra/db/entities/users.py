f78ea189c0e23a21528cc1b97df669b94c71cbbc
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-08-12 21:29:20 -0300

    feat: Implementing select_users and tests in UsersRepository

--- src/infra/db/entities/users.py
+++ src/infra/db/entities/users.py
@@ -11,4 +11,4 @@ class Users(Base):
     age = Column(Integer, nullable=False)
 
     def __repr__(self):
-        return f"Users [id={self.id}, first_name={self.first_name}]"
+        return f"Users [id={self.id}, first_name={self.first_name}, last_name={self.last_name}]"
